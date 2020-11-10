b379caa2e0ddc3685594540284e36bf0d7b68da2
jakang <jakang@wustl.edu>
2020-11-09 16:23:28 -0800

    update app.py

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -12,4 +12,24 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
-	
+	# Find one record of data from the mongo database
+    destination_data = mongo.db.collection.find_one()
+
+    # Return template and data
+    return render_template("index.html", vacation=destination_data)
+
+@app.route("/scrape")
+def scrape():
+
+    # Run the scrape function
+    mars_data = scrape_mars.scrape_info()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, mars_data, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
